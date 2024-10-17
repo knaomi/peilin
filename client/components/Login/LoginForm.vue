62933fd0e195f19fc687a4550a3511adfe68cc96
knaomi <86806301+knaomi@users.noreply.github.com>
2024-10-16 21:46:41 -0400

    Initial commit

---
+++ client/components/Login/LoginForm.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import router from "@/router";
+import { useUserStore } from "@/stores/user";
+import { ref } from "vue";
+
+const username = ref("");
+const password = ref("");
+const { loginUser, updateSession } = useUserStore();
+
+async function login() {
+  await loginUser(username.value, password.value);
+  // TODO 3: Once we log in, how can we update the currently logged in user and navigate to their profile?
+  // Hint 1: Look at this file in the original starter code to see how we currently do this for 'Home'
+  // ---------------
+  // ---------------
+  void updateSession();
+  void router.push({ name: "Profile" });
+}
+</script>
+
+<template>
+  <form class="pure-form pure-form-aligned" @submit.prevent="login">
+    <h3>Login</h3>
+    <fieldset>
+      <div class="pure-control-group">
+        <label for="aligned-name">Username</label>
+        <input v-model.trim="username" type="text" id="aligned-name" placeholder="Username" required />
+      </div>
+      <div class="pure-control-group">
+        <label for="aligned-password">Password</label>
+        <input type="password" v-model.trim="password" id="aligned-password" placeholder="Password" required />
+      </div>
+      <div class="pure-controls">
+        <button type="submit" class="pure-button pure-button-primary">Submit</button>
+      </div>
+    </fieldset>
+  </form>
+</template>
+
+<style scoped>
+h3 {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ client/stores/settings.ts
@@ -0,0 +1,49 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+import { fetchy } from "@/utils/fetchy";
+
+export const useSettingsStore = defineStore(
+  "settings",
+  () => {
+    const suppressedUsers = ref<Array<string>>([]);
+
+    const suppressUser = async (username: string) => {
+      try {
+        await fetchy(`/api/suppression/${username}`, "POST");
+      } catch {
+        return;
+      }
+      await updateSuppressedUsers();
+    };
+
+    const unsuppressUser = async (username: string) => {
+      // TODO 5: Implement this function. Hint: take a look at suppressUser.
+      try {
+        await fetchy(`/api/suppression/${username}`, "DELETE");
+      } catch {
+        return;
+      }
+      await updateSuppressedUsers();
+    };
+
+    const updateSuppressedUsers = async () => {
+      let suppressedUsersResults;
+      try {
+        suppressedUsersResults = await fetchy("/api/suppression", "GET");
+        suppressedUsers.value = suppressedUsersResults;
+      } catch (_) {
+        suppressedUsers.value = [];
+      }
+      suppressedUsers.value = suppressedUsersResults;
+    };
+
+    return {
+      suppressedUsers,
+      suppressUser,
+      unsuppressUser,
+      updateSuppressedUsers,
+    };
+  },
+  { persist: true },
+);
